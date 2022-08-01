d305ef7d29a786a0eb63c2af105e0540abe02e8d
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-08-01 18:45:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(700, 600);
     video.hide();
+    video.parent('game_console');
 
     posenet = ml5.poseNet(video, modelLoaded);
     posenet.on('pose', gotPoses);
